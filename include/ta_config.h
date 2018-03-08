8e94a5d3ed6376bac91772f6ca56335a0060d2d5
Kai Liu <nebulabox@gmail.com>
2018-03-08 20:34:56 +0800

    no message

---
+++ include/ta_config.h
@@ -0,0 +1,36 @@
+
+#define HAVE_FLOAT_H 1
+#define HAVE_INTTYPES_H 1
+#define HAVE_LIMITS_H 1
+#define HAVE_LOCALE_H 1
+#define HAVE_STDDEF_H 1
+#define HAVE_STDINT_H 1
+#define HAVE_STDLIB_H 1
+#define HAVE_STRING_H 1
+#define HAVE_UNISTD_H 1
+#define HAVE_WCHAR_H 1
+#define HAVE_WCTYPE_H 1
+#define HAVE_FLOOR 1
+#define HAVE_ISASCII 1
+#define HAVE_LOCALECONV 1
+#define HAVE_MBLEN 1
+#define HAVE_MEMMOVE 1
+#define HAVE_MEMSET 1
+#define HAVE_MODF 1
+#define HAVE_POW 1
+#define HAVE_SQRT 1
+#define HAVE_STRCASECMP 1
+#define HAVE_STRCHR 1
+#define HAVE_STRERROR 1
+#define HAVE_STRNCASECMP 1
+#define HAVE_STRRCHR 1
+#define HAVE_STRSTR 1
+#define HAVE_STRTOL 1
+#define HAVE_STRTOUL 1
+#define HAVE_PTRDIFF_T 1
+#define HAVE_SIZE_T 1
+/* #undef size_t */
+/* #undef TM_IN_SYS_TIME */
+#define HAVE_STRCOLL 1
+#define HAVE_STRFTIME 1
+#define HAVE_VPRINTF 1
